774e64fdac280eda1594d026b608bf373bf2cce4
Holger Woltersdorf <hw@hollo.me>
2018-12-02 12:30:46 +0100

    Add proper error handling with output

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ namespace hollodotme\GitHub\OrgAnalyzer;
 
 use hollodotme\GitHub\OrgAnalyzer\Infrastructure\TemplateRenderer\TemplateRenderer;
 use Throwable;
+use function error_reporting;
+use function ini_set;
+use const E_ALL;
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'On' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 

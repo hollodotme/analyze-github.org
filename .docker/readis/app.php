1cd372aba97f0e98a11f8723957feb46225748b7
Holger Woltersdorf <hw@hollo.me>
2018-12-06 23:23:22 +0100

    Add hollodotme/readis instance to dev environment

---
+++ .docker/readis/app.php
@@ -0,0 +1,5 @@
+<?php declare(strict_types=1);
+
+return [
+	'baseUrl' => 'http://readis.analyze-github.org/',
+];
---
+++ .docker/readis/servers.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+return [
+	[
+		'name'          => 'Local Redis-Server',
+		'host'          => 'redis',
+		'port'          => 6379,
+		'auth'          => null,
+		'timeout'       => 2.5,
+		'retryInterval' => 100,
+		'databaseMap'   => [],
+	],
+];
